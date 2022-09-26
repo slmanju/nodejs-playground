146f2efb3d7eec3f2def049929699d354579b3ee
Manjula Jayawardana <manjulajayawardana@gmail.com>
2022-09-27 00:05:47 +0530

    NodeJs REST CRUD with MySql
    NodeJs sample application to demonstrate REST CRUD operations with MySql. ES6 is setup with babel to use latest features of JavaScript.

--- nodejs-rest-crud-mysql/src/routes/book-router.js
+++ nodejs-rest-crud-mysql/src/routes/book-router.js
@@ -1,38 +1,14 @@
 import express from "express";
-import { BookService } from '../services/book-service';
+import { BookController } from "../controller/book-controller";
 
 const router = express.Router();
 
-const bookService = new BookService();
+const bookController = new BookController();
 
-router.get('/', (req, res) => {
-  bookService.findAll()
-    .then(books => res.json(books))
-    .catch(err => res.status(500).json({ message: 'something terrible happened' }));
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  bookService.findById(id)
-    .then(book => res.json(book))
-    .catch(err => res.status(404).json({ message: `book not found for id ${id}`}));
-});
-
-router.post('/', (req, res) => {
-  bookService.save(req.body)
-    .then(book => res.json(book))
-    .catch(err => res.status(500).json({ message: 'something terrible happened' }));
-});
-
-router.put('/:id', (req, res) => {
-  bookService.update(req.params.id, req.body)
-    .then(book => res.json(book))
-    .catch(err => res.status(500).json({ message: 'something terrible happened' }));
-});
-
-router.delete('/:id', (req, res) => {
-  bookService.delete(req.params.id);
-  res.status(204).json({ message: 'Book removed' });
-});
+router.get('/', bookController.findAll);
+router.get('/:id', bookController.findById);
+router.post('/', bookController.save);
+router.put('/:id', bookController.update);
+router.delete('/:id', bookController.delete);
 
 module.exports = router;
